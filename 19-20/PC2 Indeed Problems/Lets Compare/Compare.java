db95dfd53a4c1dde0e596181c9e0f9f55c182f07
Hollister Ream <hollister.the.great@gmail.com>
2020-02-03 18:58:25 -0600

    first commit

---
+++ 19-20/PC2 Indeed Problems/Lets Compare/Compare.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.io.*;
+
+public class Compare {
+    public static void main (String args[]) throws IOException{
+        Scanner file = new Scanner(new File("compare.dat"));
+    }
+}
