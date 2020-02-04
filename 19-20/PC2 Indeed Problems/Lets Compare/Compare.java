a92e42e33c2c401f1b589dececab3e7e5c52fc82
Hollister Ream <hollister.the.great@gmail.com>
2020-02-03 19:34:42 -0600

    first commit

--- 19-20/PC2 Indeed Problems/Lets Compare/Compare.java
+++ 19-20/PC2 Indeed Problems/Lets Compare/Compare.java
@@ -4,5 +4,21 @@ import java.io.*;
 public class Compare {
     public static void main (String args[]) throws IOException{
         Scanner file = new Scanner(new File("compare.dat"));
+
+        String comapreTo = "9th Annual Indeed Computer Science UIL Invitational";
+
+        while (file.hasNext()) {
+            String line = file.nextLine();
+
+            for (int i = 0; i < comapreTo.length() - 1 && i < line.length() - 1; i++) {
+                int x = 0;
+
+                if (line.charAt(i) == comapreTo.charAt(i)) {
+                    x++;
+                }
+
+                System.out.println(x);
+            }
+        }
     }
 }
